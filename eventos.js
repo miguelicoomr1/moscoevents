c7b7b4f90dad66b5dfbc9591d1cba7eb55a1c4b1
miguelicoomr1 <miguel26mra@gmail.com>
2026-06-15 15:40:45 +0200

    Create eventos.js

---
+++ eventos.js
@@ -0,0 +1,16 @@
+const eventos = {
+    "2026-08-09": {
+        titulo: "🔥 Operación Verano",
+        enlace: "operación-verano.html"
+    },
+
+    "2026-09-20": {
+        titulo: "🔫 Partida CQB",
+        enlace: "proximo-evento.html"
+    },
+
+    "2026-10-18": {
+        titulo: "🏆 Evento Especial",
+        enlace: "evento-especial.html"
+    }
+};
